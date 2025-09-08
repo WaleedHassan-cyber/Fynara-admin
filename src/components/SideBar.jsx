38ed0001d096754c036f7ff238e6498433b4b735
WaleedHassan-cyber <waleedhassan3722@gmail.com>
2025-09-08 19:27:54 +0500

    log

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,11 +7,16 @@ import Orders from "./Orders.jsx";
 import CreateProduct from "./Create.jsx";
 import AllProduct from "./AllProduct.jsx";
 import History from "./History.jsx";
-
+import { useNavigate } from "react-router-dom";
+import { Loader } from "lucide-react";
+import Modal from "./Modal.jsx";
 const SideBar = () => {
   const [openSide, setOpenSide] = useState(false);
   const [selectedSection, setSelectedSection] = useState("home");
-
+  const [loading, setLoading] = useState(false);
+  const { modal, setModal } = useState(false);
+  const API_URL = import.meta.env.VITE_API_URL;
+  const navigate = useNavigate();
   const openBar = () => {
     setOpenSide(true);
     const sidebar = document.querySelector(".sidebar");
@@ -24,6 +29,31 @@ const SideBar = () => {
       sidebar.classList.remove("open");
     } else {
       sidebar.classList.add("open");
+    }
+  };
+  const handleLogout = () => {
+    setLoading(true);
+    try {
+      const res = fetch(`${API_URL}/api/logout`, {
+        method: "GET",
+        credentials: "include",
+      });
+      if (res.status === 200) {
+        setLoading(false);
+        console.log("Logged out successfully");
+        localStorage.removeItem("user:token");
+        localStorage.removeItem("user:detail");
+        setModal(true);
+        setTimeout(() => {
+          setModal(false);
+        }, 2000);
+        navigate("/login");
+      } else {
+        setLoading(false);
+        console.error("Logout failed");
+      }
+    } catch (error) {
+      console.error("Error during logout:", error);
     }
   };
 
@@ -75,7 +105,7 @@ const SideBar = () => {
 
           <li onClick={() => setSelectedSection("saved")}>
             <a href="#!">
-             <i className='bx  bxs-t-shirt'  ></i> 
+              <i className="bx  bxs-t-shirt"></i>
               <span className="links_name">All Product</span>
             </a>
             <span className="tooltip">All Products</span>
@@ -104,9 +134,18 @@ const SideBar = () => {
                 <div className="job">By Waleed</div>
               </div>
             </div>
-            <i className="bx bx-log-out" id="log_out" />
+            <div className="log_out" onClick={handleLogout}>
+              <i className="bx bx-log-out" id="log_out" />
+            </div>
           </li>
         </ul>
+        {loading && <Loader />}
+        {modal && (
+          <Modal
+            success={true} // true or false
+            message="Logout"
+          />
+        )}
       </div>
 
       {/* Conditionally rendered sections */}
@@ -117,16 +156,24 @@ const SideBar = () => {
           </div>
         )}
         {selectedSection === "analytics" && (
-          <div className="text"><Analytics/></div>
+          <div className="text">
+            <Analytics />
+          </div>
         )}
         {selectedSection === "files" && (
-          <div className="text"><CreateProduct/></div>
+          <div className="text">
+            <CreateProduct />
+          </div>
         )}
         {selectedSection === "order" && (
-          <div className="text"><Orders/></div>
+          <div className="text">
+            <Orders />
+          </div>
         )}
         {selectedSection === "saved" && (
-          <div className="text"><AllProduct/></div>
+          <div className="text">
+            <AllProduct />
+          </div>
         )}
         {selectedSection === "settings" && (
           <div className="text">
